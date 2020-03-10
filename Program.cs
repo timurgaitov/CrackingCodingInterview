b58b7dd3e57a9d547c7a54d0151c9d96dae6a9ae
Timur Gaitov <timur.gaitov@gmail.com>
2020-03-10 15:25:03 +0700

    spiral array

--- Program.cs
+++ Program.cs
@@ -8,11 +8,56 @@ namespace CrackingCodingInterview
     {
         public static void Main(string[] args)
         {
-            var test = new [] { 5, 4, 1, 2, 3, 2, 19 };
+            PrintSpiral(0);
+            PrintSpiral(1);
+            PrintSpiral(2);
+            PrintSpiral(3);
+            PrintSpiral(4);
+            PrintSpiral(5);
+            PrintSpiral(6);
+        }
 
-            Console.WriteLine(string.Join(" ", test));
-            HeapSort.Sort(test);
-            Console.WriteLine(string.Join(" ", test));
+        private static void PrintSpiral(int n)
+        {
+            var spiral = new int[n, n];
+
+            var current = 1;
+
+            var lap = 0;
+
+            while (lap < n / 2 + 1)
+            {
+                for (var col = lap; col < n - lap; col++)
+                {
+                    spiral[lap, col] = current++;
+                }
+
+                for (var row = lap + 1; row < n - lap; row++)
+                {
+                    spiral[row, n - lap - 1] = current++;
+                }
+
+                for (var col = n - lap - 2; col >= lap; col--)
+                {
+                    spiral[n - lap - 1, col] = current++;
+                }
+
+                for (var row = n - lap - 2; row >= lap + 1; row--)
+                {
+                    spiral[row, lap] = current++;
+                }
+
+                lap++;
+            }
+
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    Console.Write(spiral[i, j].ToString().PadLeft(2) + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
